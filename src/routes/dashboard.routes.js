6cb4b06958fd8d356b51dba63630c87c6b372bc8
fathurrahman <fathurrahman.qarnain@datanusantara.com>
2025-05-12 22:31:39 +0700

    update dashboard

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -2,30 +2,12 @@ const { Router } = require("express");
 const router = Router();
 
 const dashboardController = require("../controllers/dashboard.controller");
-const {
-  authenticateToken,
-  isGuruOrAdmin,
-} = require("../middleware/auth.middleware");
+const { authenticateToken } = require("../middleware/auth.middleware");
 
-router.get(
-  "/metrics",
-  authenticateToken,
-  isGuruOrAdmin,
-  dashboardController.metrics,
-);
+router.get("/metrics", authenticateToken, dashboardController.metrics);
 
-router.get(
-  "/chart",
-  authenticateToken,
-  isGuruOrAdmin,
-  dashboardController.chartNilai,
-);
+router.get("/chart", authenticateToken, dashboardController.chartNilai);
 
-router.get(
-  "/calendar",
-  authenticateToken,
-  isGuruOrAdmin,
-  dashboardController.calendar,
-);
+router.get("/calendar", authenticateToken, dashboardController.calendar);
 
 module.exports = router;
